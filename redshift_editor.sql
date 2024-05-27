8d5d3b12c2aebed6d1325bd9a5d02607f4d4ec38
Abhishek Shah <abhishekshah137@gmail.com>
2024-05-28 03:47:50 +0530

    upload code

---
+++ redshift_editor.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS zillowdata (
+    bathrooms NUMERIC,
+    bedroom NUMERIC,
+    city VARCHAR(255),
+    homeStatus VARCHAR(255),
+    homeType VARCHAR(255),
+    livingArea NUMERIC,
+    price NUMERIC,
+    rentZestimate NUMERIC,
+    zipcode INT
+)
+
+SELECT * FROM zillowdata
+
+SELECT COUNT(*) FROM zillowdata
