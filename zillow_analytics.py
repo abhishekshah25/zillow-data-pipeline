14288767ac36c934c6f4a2de77965ca96e484472
Abhishek Shah <147745895+abhishekshah25@users.noreply.github.com>
2024-05-30 01:28:17 +0530

    Update code

--- zillow_analytics.py
+++ zillow_analytics.py
@@ -8,14 +8,12 @@ from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
 
 
-# Load JSON config file
 with open('/home/ubuntu/airflow/config_api.json', 'r') as config_file:
     api_host_key = json.load(config_file)
 
 now = datetime.now()
 dt_now_string = now.strftime("%d%m%Y%H%M%S")
 
-# Define the S3 bucket
 s3_bucket = 'zillow-transformed-data-bucket' 
 
 def extract_zillow_data(**kwargs):
@@ -23,25 +21,23 @@ def extract_zillow_data(**kwargs):
     headers = kwargs['headers']
     querystring = kwargs['querystring']
     dt_string = kwargs['date_string']
-    # return headers
     response = requests.get(url, headers=headers, params=querystring)
     response_data = response.json()
     
 
-    # Specify the output file path
     output_file_path = f"/home/ubuntu/response_data_{dt_string}.json"
     file_str = f'response_data_{dt_string}.csv'
 
-    # Write the JSON response to a file
+    
     with open(output_file_path, "w") as output_file:
-        json.dump(response_data, output_file, indent=4)  # indent for pretty formatting
+        json.dump(response_data, output_file, indent=4)
     output_list = [output_file_path, file_str]
     return output_list  
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 4, 15),
+    'start_date': datetime(2024, 5, 28),
     'email': ['myemail@domain.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -58,7 +54,7 @@ with DAG('zillow_analytics_dag',
         task_id= 'tsk_extract_zillow_data_var',
         python_callable=extract_zillow_data,
         op_kwargs={'url': 'https://zillow56.p.rapidapi.com/search', 'querystring': {"location":"houston, tx"}, 'headers': api_host_key, 'date_string':dt_now_string}
-        ) # Modify Query String
+        ) # Modify query accordingly
 
         load_to_s3 = BashOperator(
             task_id = 'tsk_load_to_s3',
@@ -70,9 +66,9 @@ with DAG('zillow_analytics_dag',
         bucket_key='{{ti.xcom_pull("tsk_extract_zillow_data_var")[1]}}',
         bucket_name=s3_bucket,
         aws_conn_id='aws_s3_conn',
-        wildcard_match=False,  # Set this to True if you want to use wildcards in the prefix
-        timeout=60,  # Optional: Timeout for the sensor (in seconds)
-        poke_interval=5,  # Optional: Time interval between S3 checks (in seconds)
+        wildcard_match=False, 
+        timeout=60,  
+        poke_interval=5,  
         )
         
         transfer_s3_to_redshift = S3ToRedshiftOperator(
