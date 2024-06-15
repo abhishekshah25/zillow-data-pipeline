b817e4bc76a64ea605d4efba3a51fca1d3edc9c1
Abhishek Shah <147745895+abhishekshah25@users.noreply.github.com>
2024-06-16 02:24:25 +0530

    Update Code

--- zillow_analytics.py
+++ zillow_analytics.py
@@ -38,7 +38,7 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2024, 5, 28),
-    'email': ['myemail@domain.com'],
+    'email': ['Smudgy@domain.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 2,
