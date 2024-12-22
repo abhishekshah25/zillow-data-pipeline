c8e9c291ae3e5a138c043b5be95f7bd977e4eb75
Abhishek Shah <147745895+abhishekshah25@users.noreply.github.com>
2024-12-22 19:09:40 +0530

    Update Code

--- transform-convert-to-csv-lambda.py
+++ transform-convert-to-csv-lambda.py
@@ -38,5 +38,5 @@ def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
-        'body': json.dumps('CSV file conversion & S3 upload completed successfully!')
+        'body': json.dumps('CSV file conversion & S3 upload completed successfully')
     }
